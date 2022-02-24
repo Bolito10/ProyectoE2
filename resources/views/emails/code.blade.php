179c5033e409c39377f6d6cecbceef5e873e2af1
José Luis Ibarra Llamas <luisitoibarra@live.com.mx>
2022-02-24 10:41:33 -0600

    Subida proyecto

---
+++ resources/views/emails/code.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hola Marisol</title>
+</head>
+<body>
+    <h1>{{ $details['title'] }}</h1>
+    <p>Tu codigo es : {{ $details['code'] }}</p>
+     
+    <p>Gracias</p>
+</body>
+</html>
